e70ae51a3b707a6c6f4488cb7ad948b7c3a47a0b
rapsealk <piono623@naver.com>
2020-08-03 11:47:03 +0900

    Update: ASIA -> NA

--- mods/migrate.bat
+++ mods/migrate.bat
@@ -1,12 +1,14 @@
 :: PATH to World of Warships game.
-set path="C:\Games\World_of_Warships_ASIA\bin\2697511\res_mods\0.9.6.1"
+IF NOT "%1" == "" ( SET region=%1) ELSE ( SET region=NA)
 
-if not exist %path%\PnFMods ( mkdir %path%\PnFMods )
+SET path="C:\Games\World_of_Warships_%region%\bin\2697511\res_mods\0.9.6.1"
+
+IF NOT EXIST %path%\PnFMods ( mkdir %path%\PnFMods )
 
 $null > %path%\PnFModsLoader.py
 
 echo %path%
 
-if not exist %path%\PnFMods\AutoMod ( mkdir %path%\PnFMods\AutoMod )
+IF NOT EXIST %path%\PnFMods\AutoMod ( mkdir %path%\PnFMods\AutoMod )
 
 copy Main.py %path%\PnFMods\AutoMod\Main.py
